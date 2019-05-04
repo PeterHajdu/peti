5c15526f2281c96ed1da4fe17bc4eb8ae830f022
Peter Hajdu <peter@hajdu.im>
2019-05-04 15:29:51 +0200

    add small makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+p: *.idr
+	idris -p contrib -p iterm Main.idr -o p
+
+test: *.idr
+	idris -p contrib -p specdris -p iterm Test.idr -o test
+	./test
+
+clean:
+	rm -f p test *.ibc
+
+.phony: clean
--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-fwa *.idr | while read; do idris -p iterm -p contrib ./Main.idr -o p; idris -p iterm -p contrib -p specdris ./Test.idr -o test; ./test; echo -------------------; done
+fwa *.idr | while read; do make test; echo -------------------; done
